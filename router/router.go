3343a6c5801d2769b322e157d766d7f373f33ee3
juliardimegah <juliardimegah@gmail.com>
2023-11-26 18:59:46 +0700

    another update

--- router/router.go
+++ router/router.go
@@ -11,11 +11,11 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/stock", middleware.GetAllStock).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
-	router.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/api/deletestock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTION")
+	router.HandleFunc("/api/stock", middleware.GetAllStock).Methods("GET", "OPTION")
+	router.HandleFunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTION")
+	router.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTION")
+	router.HandleFunc("/api/deletestock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTION")
 
 	return router
 }
