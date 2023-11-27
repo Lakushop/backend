2fac30544f7fdc4f4ca5a0f405ae9d4269558a25
juliardimegah <juliardimegah@gmail.com>
2023-11-27 20:04:13 +0700

    update

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	r := router.Router()
 	// fs := http.FileServer(http.Dir("build"))
 	// http.Handle("/", fs)
-	fmt.Println("Starting server on the port 8070...")
+	fmt.Println("Starting server on the port 8090...")
 
-	log.Fatal(http.ListenAndServe(":8070", r))
+	log.Fatal(http.ListenAndServe(":8090", r))
 }
